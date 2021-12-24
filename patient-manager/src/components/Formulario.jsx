82518a14c534a96993b28c0c4fba2f1a3fb8de94
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-24 15:58:52 -0600

    UseState
    Leyendo el resto de los inputs y colocandolos en el State

--- patient-manager/src/components/Formulario.jsx
+++ patient-manager/src/components/Formulario.jsx
@@ -3,7 +3,15 @@ import { useState, useEffect } from 'react';
 function Formulario() {
 
     const [ nombre, setNombre ] = useState('');
+    const [ propietario, setPropietario ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ fecha, setFecha ] = useState('');
+    const [ sintomas, setSintomas ] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Enviando formulario ...')
+    }
 
     return (
         <div className='md:w-1/2 lg:w-2/5'>
@@ -14,7 +22,10 @@ function Formulario() {
                 <span className='text-indigo-600 font-bold'>Administralos</span>
             </p>
 
-            <form action="" className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+            <form 
+                action="" 
+                onSubmit={handleSubmit}
+                className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
 
                 <div className='mb-5'>
                     <label 
@@ -38,7 +49,9 @@ function Formulario() {
                         id='propietario'
                         type="text" 
                         placeholder='Nombre del propietario'
-                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' />
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        value={propietario} 
+                        onChange={ (e) => setPropietario(e.target.value) } />
                 </div>
 
                 <div className='mb-5'>
@@ -49,7 +62,9 @@ function Formulario() {
                         id='email'
                         type="email" 
                         placeholder='E-mail Contacto Propietario'
-                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' />
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        value={email} 
+                        onChange={ (e) => setEmail(e.target.value) } />
                 </div>
 
                 <div className='mb-5'>
@@ -59,7 +74,9 @@ function Formulario() {
                     <input 
                         id='alta'
                         type="date" 
-                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' />
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        value={fecha} 
+                        onChange={ (e) => setFecha(e.target.value) } />
                 </div>
 
                 <div className='mb-5'>
@@ -69,7 +86,9 @@ function Formulario() {
                     <textarea
                         id='sintomas'
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
-                        placeholder='Describe los sintomas' />
+                        placeholder='Describe los sintomas'
+                        value={sintomas} 
+                        onChange={ (e) => setSintomas(e.target.value) } />
                 </div>
 
                 <input
--- patient-manager/src/components/Paciente.jsx
+++ patient-manager/src/components/Paciente.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Paciente() {
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-2 text-gray-700 uppercase">
         Nombre: {""}
         <span className="font-normal normal-case">Lilo</span>
@@ -16,8 +16,8 @@ function Paciente() {
       <p className="font-bold mb-2 text-gray-700 uppercase">
         E-mail: {""}
         <span className="font-normal normal-case">kakroto@gmail.com</span>
-      </p>
-
+      </p>   
+ 
       <p className="font-bold mb-2 text-gray-700 uppercase">
         Fecha de Alta: {""}
         <span className="font-normal normal-case">
