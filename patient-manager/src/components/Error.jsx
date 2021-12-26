7ffdf60a644e1db1952855b6aab3daee67417436
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-26 00:49:25 -0600

    El prop {children}

--- patient-manager/src/components/Error.jsx
+++ patient-manager/src/components/Error.jsx
@@ -1,8 +1,8 @@
 
-function Error({mensaje}) {
+function Error({children}) {
     return (
         <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
-            <p>{mensaje}</p>
+            {children}
         </div>
     )
 }
