7056475154eebc0d6f30c1dec13d7146b44fc34e
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-26 14:40:18 -0600

    Editar
    Cambiando el texto del boton al dar clic en editar

--- patient-manager/src/components/Formulario.jsx
+++ patient-manager/src/components/Formulario.jsx
@@ -150,7 +150,7 @@ function Formulario({ pacientes, setPacientes, paciente }) {
                     type="submit"
                     className='bg-indigo-600 w-full p-3 text-white uppercase font-bold 
                                 hover:bg-indigo-700 cursor-pointer transition-colors '
-                    value="Agregar Paciente" />
+                    value={paciente.id ? 'Actualizar paciente' : 'Agregar paciente'} />
 
             </form>
 
