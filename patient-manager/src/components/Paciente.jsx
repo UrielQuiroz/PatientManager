8cecb3cf5fc78362a3708b9d4e8c53dfc5db0e90
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-26 02:22:09 -0600

    Creando botones para editar y eliminar

--- patient-manager/src/components/Paciente.jsx
+++ patient-manager/src/components/Paciente.jsx
@@ -34,6 +34,18 @@ function Paciente({paciente}) {
           {sintomas}
         </span>
       </p>
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-xl"
+          >Editar</button>
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-xl"
+          >Eliminar</button>
+      </div>
     </div>
   );
 }
